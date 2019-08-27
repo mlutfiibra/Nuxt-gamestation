7192d98ef006da099195d8741cc5f075f6ba2cc9
lutfi <mlutfiibra@outlook.com>
2019-08-28 01:58:58 +0700

    Implement UserProfile page

---
+++ client/pages/user/_id/profile.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="my-card my-card--black mt-2" style="padding: 10px; width: 45em;">
+    <h5>User Profile</h5>
+    <p>Email: {{user.email}}</p>
+    <p>Address: {{user.address}}</p>
+    <p>Phone: {{user.phone}}</p>
+  </div>
+</template>
+
+<script>
+  import {
+    mapState
+  } from 'vuex';
+  export default {
+    computed: {
+      user() {
+        return this.$store.state.users.user
+      }
+    },
+  }
+
+</script>
+
+<style>
+
+</style>
